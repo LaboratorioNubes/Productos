fbd8ea831485f8ab753e8cb6e4227d0942695106
fio154 <51134863+fio154@users.noreply.github.com>
2021-05-24 18:28:02 -0300

    Creando spring

---
+++ dan-ms-productos/src/main/java/danmsproductos/danmsproductos/DanMsProductosApplication.java
@@ -0,0 +1,13 @@
+package danmsproductos.danmsproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsProductosApplication.class, args);
+	}
+
+}
