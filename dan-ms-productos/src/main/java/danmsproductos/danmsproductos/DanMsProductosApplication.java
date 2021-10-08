0f214be62eefa5aeeb304d7a46f7751c5841d6ff
Ale Guiter <aleguiter9@gmail.com>
2021-10-08 18:00:46 -0300

    Added eureka on main func

--- dan-ms-productos/src/main/java/danmsproductos/danmsproductos/DanMsProductosApplication.java
+++ dan-ms-productos/src/main/java/danmsproductos/danmsproductos/DanMsProductosApplication.java
@@ -2,8 +2,10 @@ package danmsproductos.danmsproductos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DanMsProductosApplication {
 
 	public static void main(String[] args) {
